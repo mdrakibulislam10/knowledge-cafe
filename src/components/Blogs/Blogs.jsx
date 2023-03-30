932b62d45125d972f0e7df378ecbcefca2b9b1cc
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-03-31 04:50:24 +0600

    font awesome

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import "./Blogs.css";
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blogs = (props) => {
     console.log(props.blog);
+    const { images, author, title, read_time, hash, publish_date } = props.blog;
     return (
-        <div className='blog'>
+        <section className='blog'>
+            <img className='cover-img' src={images.cover} alt="" />
 
-        </div>
+            <div>
+                <div>
+                    <img src={images.author} alt="" />
+                    <h3>{author}</h3>
+                    <p>{publish_date}</p>
+                </div>
+                <div>
+                    <p>{read_time} min read <FontAwesomeIcon icon={faBookmark} /></p>
+                </div>
+            </div>
+
+        </section>
     );
 };
 
