311eed6ab57ee8d77349bf52d2519050eb020bec
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-03-31 11:26:44 +0600

    al blogs card display in UI

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -11,17 +11,28 @@ const Blogs = (props) => {
         <section className='blog'>
             <img className='cover-img' src={images.cover} alt="" />
 
-            <div>
-                <div>
-                    <img src={images.author} alt="" />
-                    <h3>{author}</h3>
-                    <p>{publish_date}</p>
+            <div className='author-info'>
+                <div className='author'>
+                    <img className='author-img' src={images.author} alt="" />
+                    <div>
+                        <h3 className='author-name'>{author}</h3>
+                        <p className='publish-date'>{publish_date}</p>
+                    </div>
                 </div>
                 <div>
-                    <p>{read_time} min read <FontAwesomeIcon icon={faBookmark} /></p>
+                    <p className='read-time'>{read_time} min read
+                        <span> <FontAwesomeIcon icon={faBookmark} /></span>
+                    </p>
                 </div>
             </div>
 
+            <div style={{ textAlign: "start" }}>
+                <h2 className='title'>{title}</h2>
+                <p className='hash'><span>{hash[0]}</span> <span>{hash[1]}</span></p>
+                <a className='mark-as-read-btn' href="">Mark as read</a>
+            </div>
+
+            <hr />
         </section>
     );
 };
